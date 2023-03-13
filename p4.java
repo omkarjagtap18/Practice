5d54878786e0c95278d14c367de776d988921213
Omkar <omkarvj1817@gmail.com>
2023-03-13 15:57:58 +0530

    pattern 4

---
+++ p4.java
@@ -0,0 +1,14 @@
+public class p4 {
+    public static void main(String[] args) {
+        int i,j,k;
+        for(i=1; i<=5; i++){
+            for(j=1; j<i; j++){
+                System.out.print(" ");
+            }
+            for(k=i; k<=5; k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
