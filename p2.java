5dca38b1a3f4b0a3649b6ae172b98e4f5b49ac17
Omkar <omkarvj1817@gmail.com>
2023-03-13 14:57:21 +0530

    pattern 2

--- p2.java
+++ p2.java
@@ -1,8 +1,8 @@
-class p1{
+public class p2 {
     public static void main(String[] args) {
         int i,j;
-        for(i=1;i<=5;i++){    //Row
-            for(j=1;j<=i;j++){    //column
+        for(i=1; i<=5; i++){
+            for(j=5; j>=i; j--){
                 System.out.print("*");
             }
             System.out.print("\n");
